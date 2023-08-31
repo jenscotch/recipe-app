56ea055e5fb01789d2bc7904fa448736307c2deb
jenscotch <cartooner777@yahoo.com>
2023-08-31 16:30:27 -0700

    added recipe grid, recipe images, styling

--- recipes/models.py
+++ recipes/models.py
@@ -1,19 +1,28 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
-
-difficulty_level = (
-    ('easy', 'Easy'),
-    ('medium', 'Medium'),
-    ('intermediate', 'Intermediate'),
-    ('hard', 'Hard'),
-)
 
 class Recipe(models.Model):
     name = models.CharField(max_length=120)
     cooking_time = models.FloatField(help_text='in minutes')
     ingredients = models.CharField(max_length=500)
-    difficulty = models.CharField(max_length=12, choices=difficulty_level, default='ez')
+    pic = models.ImageField(upload_to='recipes', default='no_image.jpg')
 
     def __str__(self):
         return str(self.name)
+    
+    def calculate_difficulty(self):
+        ingredients = self.ingredients.split(', ')
+        if self.cooking_time < 25 and len(ingredients) < 7:
+            difficulty = 'Easy'
+        elif self.cooking_time < 25 and len(ingredients) >= 7:
+            difficulty = 'Medium'
+        elif self.cooking_time >= 25 and len(ingredients) < 7:
+            difficulty = 'Intermediate'
+        elif self.cooking_time >= 25 and len(ingredients) >= 7:
+            difficulty = 'Hard'
+        return difficulty
+    
+    def get_absolute_url(self):
+        return reverse('recipes:detail', kwargs={'pk': self.pk})
