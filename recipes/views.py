9b4a2e8dddaaa3f4443d02d82027c250c4c3bcdd
jenscotch <cartooner777@yahoo.com>
2023-09-03 12:13:19 -0700

    added user authentication features

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def home(request):
     return render(request, 'recipes/recipes_home.html')
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipes/main.html'
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipes/detail.html'
