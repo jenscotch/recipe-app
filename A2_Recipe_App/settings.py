232bcaab3556210ebdfe4f172252f195669341f6
jenscotch <cartooner777@yahoo.com>
2023-09-04 15:25:24 -0700

    debug image storage

--- A2_Recipe_App/settings.py
+++ A2_Recipe_App/settings.py
@@ -147,6 +147,9 @@ LOGIN_URL='/login/'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
