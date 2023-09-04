f13a3ac3648eed431a7cfd5909655fdab952aa5c
jenscotch <cartooner777@yahoo.com>
2023-09-04 12:25:19 -0700

    fixing bugs

--- A2_Recipe_App/settings.py
+++ A2_Recipe_App/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'recipes/static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR / "staticfiles"
